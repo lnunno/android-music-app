0c7f6c4c394b01f88eae12ad223da4a03a9a11fc
Lucas <lucasnunno@gmail.com>
2015-03-17 12:07:18 -0600

    Trying to get the drawer to open with app icon. May wait for this until necessary.

--- app/src/main/java/com/lnunno/musicapp/MainActivity.java
+++ app/src/main/java/com/lnunno/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lnunno.musicapp;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -15,6 +18,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(drawerToggle);
+
         ListView hot_artist_view = (ListView) findViewById(R.id.hotartists_listview);
         ArtistAdapter artistAdapter = new ArtistAdapter(this);
         hot_artist_view.setAdapter(artistAdapter);

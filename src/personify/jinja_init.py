b51e8f428a6a9f4aa684bfe53146594447df6352
Lucas Nunno <lucasnunno@gmail.com>
2015-04-05 20:21:23 -0600

    Version bump.

--- src/personify/jinja_init.py
+++ src/personify/jinja_init.py
@@ -7,7 +7,9 @@ from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
 from echo_nest import utils
 from personify import __version__
+from echo_nest.buckets import search_artist_buckets
 
 env = Environment(loader=FileSystemLoader('templates'))
 env.globals.update(echonest_util=utils)
 env.globals.update(__version__=__version__)
+env.globals.update(search_artist_buckets=search_artist_buckets)
